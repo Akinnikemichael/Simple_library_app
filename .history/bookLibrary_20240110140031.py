2f55ce00064899abe63b2cbc23a6a059d7b22ee1
Akinnikemichael <michaelaakinnike@gmail.com>
2024-01-10 14:01:23 +0100

    9th day commit

---
+++ .history/bookLibrary_20240110140031.py
@@ -0,0 +1,12 @@
+class book:
+
+ def __init__(self,  book_name, book_author, book_serial_number, book_type, shelf_number,  room_number, building_floor, is_book_borrowed_returned_status):
+
+  self.book_name = book_name
+  self.book_author = book_author
+  self.book_serial_number = book_serial_number
+  self.book_type = book_type
+  self.shelf_number = shelf_number
+  self. room_number = room_number
+  self.building_floor = building_floor
+  self.is_book_borrowed_returned_status = is_book_borrowed_returned_status
